bb0425405d56bb2c7ca5ca58a368d0139a88722d
ping58972 <ping58972@gmail.com>
2019-06-04 01:51:16 -0500

    implement adding cart to user

--- modles/user.js
+++ modles/user.js
@@ -2,13 +2,35 @@ const mongodb = require('mongodb');
 const getDb = require('../util/database').getDb;
 
 class User {
-    constructor(username, email) {
+    constructor(username, email, cart, id) {
         this.name = username;
         this.email = email;
+        this.cart = cart;
+        this._id =  id;
     }
 
     save() {
         return getDb().collection("users").insertOne(this).then(result => console.log(result)).catch(err=> console.log(err) );
+    }
+
+    addToCart (product) {
+        const cartProductIndex = this.cart.items.findIndex(cp => {
+            return cp.productId.toString() === product._id.toString();
+        });
+        let newQuantity = 1;
+        const updatedCartItems = [...this.cart.items];
+        if(cartProductIndex >= 0) {
+            newQuantity = this.cart.items[cartProductIndex].quantity + 1;
+            updatedCartItems[cartProductIndex].quantity = newQuantity;
+        } else {
+            updatedCartItems.push({productId: new mongodb.ObjectId(product._id), quantity: 1});
+        }
+        const updatedCart = {items: updatedCartItems};
+        return getDb().collection('users').updateOne({_id: new mongodb.ObjectId(this._id)}, {$set: {cart: updatedCart}});
+    }
+
+    getCart() {
+        return getDb().collection('products').find({_id: {$in: []}});
     }
 
     static findById(userId) {
